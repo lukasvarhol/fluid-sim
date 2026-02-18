259c505a875a3895c4e342323e543cc4a7a14bed
Lukas <luka.varhol@gmail.com>
2026-02-18 20:48:18 +0100

    [feat]: draw window with OpenGL and select background colour

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include "config.h"
+
+int main() {
+
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Fluid Sim", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
